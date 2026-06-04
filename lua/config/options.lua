ceccf13264f329235473e15d5040c1c432b27d52
Kim Bum June <bumjune.kim@navercorp.com>
2026-06-04 17:02:47 +0900

    nvim 외부에서 편집 실행시 수정 바로 반영하도록 적용

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable SpellCheck
 vim.opt.spell = false
+
+-- Auto reload files changed outside of Neovim
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "checktime",
+})
