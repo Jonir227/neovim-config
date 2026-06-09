b50fa3f12ab47f24b8be1fc0497289fc7dbc5b12
Kim Bum June <bumjune.kim@navercorp.com>
2026-06-09 19:16:57 +0900

    line blame 표시하도록 설정

--- lua/plugins/settings.lua
+++ lua/plugins/settings.lua
@@ -63,4 +63,17 @@ return {
       },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 라인 끝에 표시
+        delay = 1000,
+        ignore_whitespace = true,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+    },
+  },
 }
